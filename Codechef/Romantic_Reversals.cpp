bc0f6b4eef8ce3847faa4db957208d69eefa00c2
abdulqadir18 <boxwalaabdulqadir2002@gmail.com>
2022-01-05 00:50:05 +0530

    4jan

---
+++ Codechef/Romantic_Reversals.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+typedef long long ll;
+
+ll t;
+cin>>t;
+while(t--)
+{
+  ll n, k;
+  cin>>n>>k;
+  string s;
+  cin>>s;
+
+  // reverse(s.begin(),s.end());
+  // for(ll i=n-k; i<n-1; i++)
+  // {
+  //   reverse(s.begin()+i, s.end());
+  // }
+  // reverse(s.begin(), s.end());
+  // cout<<s<<"\n";
+}
+
+return 0;
+}
