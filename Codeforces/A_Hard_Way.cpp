f1b8558727088bbfaf367298936988447c82ad8a
abdulqadir18 <boxwalaabdulqadir2002@gmail.com>
2022-03-17 18:30:36 +0530

    17mar

--- Codeforces/A_Hard_Way.cpp
+++ Codeforces/A_Hard_Way.cpp
@@ -12,9 +12,12 @@ while(t--)
 {
   ll a[3],b[3];
   for(ll i=0; i<3; i++) cin>>a[i]>>b[i];
-  if(b[0]==b[1]) cout<<b[0]<<"\n";
-  else if(b[1]==b[2]) cout<<b[1]<<"\n";
-  else if(b[2]==b[0]) cout<<b[2]<<"\n";
+  ll x=0, y=-1, ans=0;
+  if(b[0]==b[1]) {y=b[0]; x=b[2]; ans=abs(a[0]-a[1]);}
+  else if(b[1]==b[2]) {y=b[1]; x=b[0]; ans=abs(a[1]-a[2]);}
+  else if(b[2]==b[0]) {y=b[2]; x=b[1]; ans=abs(a[0]-a[2]);}
+
+  if(y!=-1 && y>x) cout<<ans<<"\n";
   else cout<<0<<"\n";
 }
 
